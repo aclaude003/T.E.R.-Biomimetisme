7de7fc31801d707443ad1a08e308e8ca2e07b820
Yassir <myousdid.19@gmail.com>
2023-02-09 16:37:33 +0100

    program

---
+++ program/constantes_mod.f90
@@ -0,0 +1,12 @@
+module constantes_mod
+  implicit none
+  integer,parameter::PR=8,m=25 !m le nombre de particules fluide au dessus de l'axe des abscisses
+  real(PR),parameter::pi=4*atan(1._PR)
+
+
+
+
+
+
+
+end module
